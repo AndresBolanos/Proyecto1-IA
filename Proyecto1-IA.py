dad8bac0daa550979341dca99c9908b2f9f69d0d
AndresBolanos <andres.bolanos9797@gmail.com>
2018-03-01 17:26:47 -0600

    Actualizacion con la funcion de Hinge-loss aplicada al algoritmo

--- Proyecto1-IA.py
+++ Proyecto1-IA.py
@@ -10,12 +10,19 @@ def Load_Irirs_Data():
 
     return train_data
 
+#Funcion para cargar los indices de los resultados de Iris
+def Load_Iris_Index():
+    iris = load_iris()
+    train_index = iris.target
+    return train_index
+
 #Funcion para cargar los nombres de Iris
 def Load_Iris_Names():
     iris = load_iris()
     train_names = iris.target_names
     return train_names
 
+#Funcion para generar el W aleatorio
 def Generate_W(filas, columnas):
     #Genero la matriz aleatoria con numeros entre 1 y 255
     #con la cantidad de filas y  columnas que recibe como parametro la funcion
@@ -26,17 +33,25 @@ def Generate_W(filas, columnas):
 # Funcion para multiplicar cada elemento de iris
 # por el w generado aleatoriamente
 def Compare_Iris_Data():
+    L = 0; #Contiene la sumatoria de aplicar hinge-loss a cada elemento
     X = Load_Irirs_Data() # Se trae todos los datos de iris
-    Class = Load_Iris_Names() # Se trae todos los labels de iris
-    W = Generate_W(len(X[0]), len(Class)+1) #Genera e w aleatorio
-    numer_Items = len(X) #Largo de los datos
-    Result_Array = [[0,0,0,0]] #No puedo inicializarlo vacio
+    Index = Load_Iris_Index() # Se trae todos los indices de resultado de iris
+    Class = Load_Iris_Names()# Se trae todos los nombres de iris
+    W = Generate_W(len(X[0]), len(Class)+1) #Genera el w aleatorio
+    number_Items = len(X) #Largo de los datos
     #Aplicamos la multiplicacion para cada uno de los elementos del data set
-    for i in range(numer_Items):
-        R = np.dot(W, X[i,:])
-        Result_Array = np.append(Result_Array, [R], axis=0)
-    Result_Array = np.delete(Result_Array, 0, 0) #Elimino la primera lista de ceros
-    print(Result_Array)
+    for i in range(number_Items):
+        R = np.dot(W, X[i])
+        L = L+Hinge_Loss(R,Index[i]) #No se como mandar el correcto
+    print("Promedio de perdida "+str(L/number_Items)) 
+
+#Recibe el vector s y la posicion del yi de la clase optimo del arreglo s
+def Hinge_Loss(s,yi):
+    hinge_loss = 0;
+    for i in range(len(s)):
+        if (i != yi):
+            hinge_loss += np.sum(np.maximum(0,s[i]-s[yi]+1),axis=0)
+    return hinge_loss
 
 Compare_Iris_Data()
 
